f2a6c204f8586fceff1d4f346b5f317d8b4cfe32
han-yohan <gksdygks359@naver.com>
2024-12-11 23:31:31 +0900

    feat: ci-front 작업

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export', // Static HTML Export 활성화
+  images: {
+    unoptimized: true, // Image Optimization 비활성화
+  },
+  reactStrictMode: true
 };
 
 module.exports = nextConfig;
